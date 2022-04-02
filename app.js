a1d1781a13f4e73c3435f5837532cb0e3ef7cb29
rich <riky_8065@hotmail.com>
2022-04-02 15:41:53 -0600

    Es la primer parte de mi proyecto en mongo

---
+++ app.js
@@ -0,0 +1,21 @@
+const express= require ('express')
+const {ApolloServer} = require('apollo-server-express')
+
+require(')
+
+const app = express()
+
+module.exports = app
+
+function start() {
+
+    new ApolloServer{
+
+    }
+
+
+    app.listen(3000, () => {
+        console.log('Server on port', 3000)
+    })
+}
+start()
